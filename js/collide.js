4859161e860489f1d2e0065ab4248eccba6f8178
MilesIsMuchoCoolio <mjensen_msasd@magnoliascience.org>
2026-09-22 19:56:27 -0700

    Tighten spike collision so it matches the visible spike shapes instead of full-tile hits.

--- js/collide.js
+++ js/collide.js
@@ -1,2 +1,63 @@
-var Collide={};Collide.squaresUnder=function(x,y,w,h){var a=[];for(var r=Math.floor(y/CONFIG.TILE);r<=Math.floor((y+h-1)/CONFIG.TILE);r++)for(var c=Math.floor(x/CONFIG.TILE);c<=Math.floor((x+w-1)/CONFIG.TILE);c++)a.push({col:c,row:r});return a;};
-Collide.hitsSolid=function(x,y,w,h){var a=Collide.squaresUnder(x,y,w,h);for(var i=0;i<a.length;i++)if(Level.isSolid(a[i].col,a[i].row))return true;return false;};Collide.hitsSpike=function(x,y,w,h){var a=Collide.squaresUnder(x,y,w,h);for(var i=0;i<a.length;i++)if(Level.isSpike(a[i].col,a[i].row))return true;return false;};Collide.hitsFinish=function(x,y,w,h){var a=Collide.squaresUnder(x,y,w,h);for(var i=0;i<a.length;i++)if(Level.isFinish(a[i].col,a[i].row))return true;return false;};
+var Collide = {};
+
+Collide.squaresUnder = function (x, y, w, h) {
+  var squares = [];
+  var startCol = Math.floor(x / CONFIG.TILE);
+  var endCol = Math.floor((x + w - 1) / CONFIG.TILE);
+  var startRow = Math.floor(y / CONFIG.TILE);
+  var endRow = Math.floor((y + h - 1) / CONFIG.TILE);
+
+  for (var row = startRow; row <= endRow; row++) {
+    for (var col = startCol; col <= endCol; col++) {
+      if (Level.charAt(col, row) !== ".") {
+        squares.push({ col: col, row: row });
+      }
+    }
+  }
+
+  return squares;
+};
+
+Collide.hitsSolid = function (x, y, w, h) {
+  var squares = Collide.squaresUnder(x, y, w, h);
+  for (var i = 0; i < squares.length; i++) {
+    if (Level.isSolid(squares[i].col, squares[i].row)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+Collide.hitsSpike = function (x, y, w, h) {
+  var squares = Collide.squaresUnder(x, y, w, h);
+
+  for (var i = 0; i < squares.length; i++) {
+    var square = squares[i];
+    if (!Level.isSpike(square.col, square.row)) {
+      continue;
+    }
+
+    var tileX = square.col * CONFIG.TILE;
+    var tileY = square.row * CONFIG.TILE;
+    var spikeLeft = tileX + 5;
+    var spikeRight = tileX + CONFIG.TILE - 5;
+    var spikeTop = tileY + 12;
+    var spikeBottom = tileY + CONFIG.TILE - 2;
+
+    if (x < spikeRight && x + w > spikeLeft && y < spikeBottom && y + h > spikeTop) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+Collide.hitsFinish = function (x, y, w, h) {
+  var squares = Collide.squaresUnder(x, y, w, h);
+  for (var i = 0; i < squares.length; i++) {
+    if (Level.isFinish(squares[i].col, squares[i].row)) {
+      return true;
+    }
+  }
+  return false;
+};
