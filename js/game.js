06bf8aea53a74bf8810d3499c7a51939ff3070b5
MilesIsMuchoCoolio <mjensen_msasd@magnoliascience.org>
2026-09-24 11:06:49 -0700

    Add level selection and navigation for bonus levels

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ var Game = {
 };
 
 Game.startLevel = function (levelNumber) {
+  if (!Level.levels || !Level.levels[levelNumber]) return;
   Game.levelNumber = levelNumber;
   Level.build(levelNumber);
   Crumble.reset();
@@ -13,7 +14,29 @@ Game.startLevel = function (levelNumber) {
   Game.score = 0;
   Game.mode = "playing";
   Game.showMessage("");
+  Game.updateLevelMenu();
   Game.updateHUD();
+};
+
+Game.setupLevelMenu = function () {
+  var menu = document.getElementById("level-select");
+  if (!menu || !Level.levels) return;
+  menu.innerHTML = "";
+  for (var i = 0; i < Level.levels.length; i++) {
+    var option = document.createElement("option");
+    option.value = i;
+    option.textContent = (i + 1) + ". " + Level.levels[i].name;
+    menu.appendChild(option);
+  }
+  menu.onchange = function () {
+    Game.startLevel(Number(menu.value));
+  };
+  Game.updateLevelMenu();
+};
+
+Game.updateLevelMenu = function () {
+  var menu = document.getElementById("level-select");
+  if (menu) menu.value = String(Game.levelNumber);
 };
 
 Game.showMessage = function (text) {
@@ -25,7 +48,7 @@ Game.updateHUD = function () {
   var score = document.getElementById("score");
   if (!score) return;
   var reloadText = Combat.reloadTimer > 0 ? " | RELOADING" : "";
-  score.textContent = "Coins: " + Game.score + " | Ammo: " + Combat.ammo + "/" + Combat.reserve + reloadText;
+  score.textContent = "Level " + (Game.levelNumber + 1) + ": " + Level.name + " | Coins: " + Game.score + " | Ammo: " + Combat.ammo + "/" + Combat.reserve + reloadText;
 };
 
 Game.update = function () {
@@ -48,9 +71,22 @@ Game.update = function () {
 
   if (Player.hasWon()) {
     Game.mode = "won";
-    Game.showMessage("You made it! Press R to play again.");
-    return;
+    if (Game.levelNumber < Level.levels.length - 1) {
+      Game.showMessage("Level complete! Choose the next level above or press N.");
+    } else {
+      Game.showMessage("You beat every level! Press R to play again.");
+    }
   }
+};
+
+Game.nextLevel = function () {
+  if (Level.levels && Game.levelNumber < Level.levels.length - 1) {
+    Game.startLevel(Game.levelNumber + 1);
+  }
+};
+
+Game.previousLevel = function () {
+  if (Game.levelNumber > 0) Game.startLevel(Game.levelNumber - 1);
 };
 
 Game.loop = function () {
